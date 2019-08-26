3d01669e8fd711dc28ad4435afb70314ca426d6f
melissa kobia <melissakobia1997@gmail.com>
2019-08-26 11:28:41 +0300

    Adds a hover effect to the portfolio images

--- js/scripts.js
+++ js/scripts.js
@@ -44,6 +44,136 @@ $(document).ready(function () {
 
 
 
+$(document).ready(function () {
 
+  //Toggles the design image
+  $("img#designImage").click(function () {
+    $("#designShow").toggle("slow");
+    $("#designHidden").toggle("slow");
+
+  });
+
+  $("#designHidden").click(function () {
+    $("#designShow").toggle("slow");
+    $("#designHidden").toggle("slow");
+
+  });
+
+  $("img#developmentImage").click(function () {
+    $("#devShow").toggle("slow");
+    $("#devHidden").toggle("slow");
+
+  });
+
+  $("#devHidden").click(function () {
+    $("#devShow").toggle("slow");
+    $("#devHidden").toggle("slow");
+
+  });
+
+  $("img#managementImage").click(function () {
+    $("#productShow").toggle("slow");
+    $("#productHidden").toggle("slow");
+
+  });
+
+  $("#productHidden").click(function () {
+    $("#productShow").toggle("slow");
+    $("#productHidden").toggle("slow");
+
+  });
+
+  // $("#img1").hover(function(){
+  //   $("#imghidden1").show();
+  //   }, function(){
+  //   $("#img1").show();
+  // });
+
+  // $("#img1").hover(
+  //   function(){
+  //     $("#img1").animate({opacity: ".3"}, 400);
+  //     $("#imghidden1").show().animate({top:"55px"}, 0);
+  //   },
+  //   function(){
+  //     $("#img1").animate({opacity: "1.0"}, 400);
+  //     $("#imghidden1").hide().animate({top:"85px"}, 0);
+  //   }
+  // );
+
+  $("#img1").hover(function () {
+    $("#img1").mouseover(function () {
+      $("#imgHidden1").show();
+    });
+    $("#img1").mouseout(function () {
+      $("#imgHidden1").hide();
+    });
+  });
+
+  $("#img2").hover(function () {
+    $("#img2").mouseover(function () {
+      $("#imgHidden2").show();
+    });
+    $("#img2").mouseout(function () {
+      $("#imgHidden2").hide();
+    });
+  });
+
+  $("#img3").hover(function () {
+    $("#img3").mouseover(function () {
+      $("#imgHidden3").show();
+    });
+    $("#img3").mouseout(function () {
+      $("#imgHidden3").hide();
+    });
+  });
+
+  $("#img4").hover(function () {
+    $("#img4").mouseover(function () {
+      $("#imgHidden4").show();
+    });
+    $("#img4").mouseout(function () {
+      $("#imgHidden4").hide();
+    });
+  });
+
+  $("#img5").hover(function () {
+    $("#img5").mouseover(function () {
+      $("#imgHidden5").show();
+    });
+    $("#img5").mouseout(function () {
+      $("#imgHidden5").hide();
+    });
+  });
+
+  $("#img6").hover(function () {
+    $("#img6").mouseover(function () {
+      $("#imgHidden6").show();
+    });
+    $("#img6").mouseout(function () {
+      $("#imgHidden6").hide();
+    });
+  });
+
+  $("#img7").hover(function () {
+    $("#img7").mouseover(function () {
+      $("#imgHidden7").show();
+    });
+    $("#img7").mouseout(function () {
+      $("#imgHidden7").hide();
+    });
+  });
+
+  $("#img8").hover(function () {
+    $("#img8").mouseover(function () {
+      $("#imgHidden8").show();
+    });
+    $("#img8").mouseout(function () {
+      $("#imgHidden8").hide();
+    });
+  });
+
+
+
+});
 
 
